4d161c099dc3f3a46c19d6add23b72021afb3467
thulisile98 <thulisile.ma98@gmail.com>
2023-06-19 20:24:18 +0200

    added labels and input for my personal details       added two buttons  and styled them and routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SurveyComponent } from './survey/survey.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'survey', component: SurveyComponent }
 ];
 
 @NgModule({
